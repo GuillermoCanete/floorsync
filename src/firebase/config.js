59b90386d851a81fce6d68c4b0cc0f0e208fd85d
GuillermoCanete <guillermo.canete@hotmail.com>
2026-06-17 00:23:48 -0300

    chore: trigger deployment after adding secrets

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+// Firebase configuration loaded from env variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
